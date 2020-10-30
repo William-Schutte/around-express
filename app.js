d3cfcb412e0a850f370de1d039fff283f35488d3
William Schutte <williamdschutte@gmail.com>
2020-10-30 19:53:22 -0400

    Cors origin via regex for frontend sites

--- app.js
+++ app.js
@@ -21,9 +21,8 @@ mongoose.connect('mongodb://localhost:27017/aroundb', {
 });
 
 const corsOptions = {
-  origin: ['https://ws.p15.students.nomoreparties.site/', 'https://www.ws.p15.students.nomoreparties.site/'],
+  origin: (/https:\/\/(www\.)?ws\.p15\.students\.nomoreparties\.site\S*/gm),
   credentials: true,
-  preflightContinue: true,
 };
 
 app.use(cors(corsOptions));
