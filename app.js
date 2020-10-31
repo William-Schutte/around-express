23b6cf2c16c320023e20d1d3d264c05087cafd05
William Schutte <williamdschutte@gmail.com>
2020-10-30 20:23:17 -0400

    Cors options

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.post('/signup', bodyParser.json(), createUser);
 // The following routes WILL require a user to be logged in and authenticated
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/users', usersRouter);
+app.use('/users', cors(), usersRouter);
 app.use('/cards', cardsRouter);
 app.get('*', (req, res) => {
   res.status(404).send({ message: 'Requested resource not found' });
